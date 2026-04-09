0f08a663064e0be52d482c4469d183815efa662f
bybellacooks <bybellacooks@gmail.com>
2026-04-09 19:37:45 -0400

    Create site.js

---
+++ assets/js/site.js
@@ -0,0 +1,12 @@
+/* By Bella Cooks - site.js */
+
+(function () {
+  "use strict";
+
+  var root = document.documentElement;
+  root.classList.add("js");
+
+  window.addEventListener("load", function () {
+    document.body.classList.add("is-loaded");
+  });
+})();
